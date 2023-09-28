d39fa61662c718e66de404e08dd0d343b32eb03d
Harsh Sangwan <84121947+harsh-sangwan2003@users.noreply.github.com>
2023-09-28 14:25:38 +0530

    added

--- Season 2/02_Promises/index.js
+++ Season 2/02_Promises/index.js
@@ -1,12 +1,21 @@
-const cart = ["shoes", "kurta", "pants"];
+// const cart = ["shoes", "kurta", "pants"];
 
-createOrder(cart, function (orderId) {
+// createOrder(cart, function (orderId) {
 
-    proceedToPayment(orderId);
-})
+//     proceedToPayment(orderId);
+// })
 
-const promise = createOrder(cart);
-promise.then(function (orderId) {
+// const promise = createOrder(cart);
+// promise.then(function (orderId) {
 
-    proceedToPayment(orderId);
+//     proceedToPayment(orderId);
+// })
+
+const GITHUB_API = "https://api.github.com/users/harsh-sangwan2003";
+
+const user = fetch(GITHUB_API);
+
+user.then(function(data){
+
+    console.log(data);
 })
