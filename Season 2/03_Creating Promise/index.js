408d4a5aaf94b1267cda0173cc5f98e407d2ca23
Harsh Sangwan <84121947+harsh-sangwan2003@users.noreply.github.com>
2023-10-01 14:02:02 +0530

    promise-chaining

--- Season 2/03_Creating Promise/index.js
+++ Season 2/03_Creating Promise/index.js
@@ -1,2 +1,50 @@
-const cart = ["kurta","pants","shoes"];
+const cart = ["kurta", "pants", "shoes"];
 
+const promise = createOrder(cart);
+
+promise.then(function (orderId) {
+
+    console.log(orderId);
+    return orderId;
+
+}).then(function () {
+
+    return proceedToPayment(orderId);
+
+}).then(function (paymentInfo) {
+
+    console.log(paymentInfo);
+
+}).catch(err => {
+
+    console.log("Error is : ", err);
+}).then(function () {
+
+    console.log("I will be definitely be called.");
+})
+
+function validateCart(cart) {
+    return true;
+}
+
+function proceedToPayment(orderId) {
+
+    return new Promise((resolve, reject) => {
+
+        resolve("Payment successful");
+    })
+}
+
+function createOrder(cart) {
+
+    return new Promise((resolve, reject) => {
+
+        if (!validateCart(cart))
+            reject();
+
+        const orderId = "12345";
+
+        if (orderId)
+            resolve(orderId);
+    })
+}
