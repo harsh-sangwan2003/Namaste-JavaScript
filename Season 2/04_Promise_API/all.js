cf05666eb547e1c5b610119fb1dbc95d5e4f5098
harsh-sangwan2003 <hsangwan2002@gmail.com>
2024-01-21 18:44:41 +0530

    added

---
+++ Season 2/04_Promise_API/all.js
@@ -0,0 +1,39 @@
+function fn1() {
+
+    return new Promise((resolve, reject) => {
+
+        setTimeout(() => {
+            resolve(1000);
+        }, 3000)
+    })
+}
+
+function fn2() {
+
+    return new Promise((resolve, reject) => {
+
+        setTimeout(() => {
+            reject(2000);
+        }, 2000)
+    })
+}
+
+function fn3() {
+
+    return new Promise((resolve, reject) => {
+
+        setTimeout(() => {
+            resolve(3000);
+        }, 1000)
+    })
+}
+
+const p1 = fn1();
+const p2 = fn2();
+const p3 = fn3();
+
+Promise.all([p1, p2, p3]).then(val => {
+    console.log(val);
+}).catch(err => {
+    console.log(err);
+})
